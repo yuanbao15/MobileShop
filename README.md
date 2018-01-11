83e6c6bbb91ec1dd4c3ca282d013ff9facf8cbc1
yuanbao15 <yuanbao15@163.com>
2018-01-11 21:02:25 +0800

    展示

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 手机商城实战项目android  --yuanbao
 
 ### app界面截图
- ![Screenshot1](yb_images/Screenshot_1.png)
+ ![Screenshot1](yb_images/Screenshot_1.png =450x800)
  
  ![Screenshot2](yb_images/Screenshot_2.png)
  
--- app/src/main/java/com/idaniu/maga/shopping/Constant.java
+++ app/src/main/java/com/idaniu/maga/shopping/Constant.java
@@ -11,12 +11,15 @@ public class Constant {
     public static final String HEAD_URL = "http://www.yuanbao15.club/ybfile/bannerimage.json";     //yb自己的....成功了。注意json数据格式
 
 
-    //    主页瀑布流的网络资源地址
+    //主页瀑布流的网络资源地址
 //    public static final String HOME_URL = "http://112.124.22.238:8081/course_api/campaign/recommend";   //初始
     public static final String HOME_URL = "http://www.yuanbao15.club/ybfile/home.json";     //yb自己的
 
     public static final String HOST = "http://train.dolphin.com";
-    public static final String HOT_URL = HOST + "/course_api/wares/hot";    //热卖页信息
+    //热卖页瀑布流的网络资源地址
+//    public static final String HOT_URL = HOST + "/course_api/wares/hot";                  //这是之前的，现在这个服务业面关闭了
+    public static final String HOT_URL = "http://www.yuanbao15.club/ybfile/hot.json";       //yb自己的
+
     public static final String LOGIN_URL = HOST + "/course_api/auth/login";     //登录url；
     public static final String REGISTER_URL = HOST + "/course_api/auth/reg";    //注册url；
     public  static final String DES_KEY="12345670";     //加密算法的密钥。
